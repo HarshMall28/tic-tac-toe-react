fae80942dddc9fc765d7fd51d078054467293e29
harshmall28 <contact.harshmall@gmail.com>
2025-03-06 22:37:06 +0000

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/tic-tac-toe-react",
   build: {
     rollupOptions: {
       input: "src/index.jsx", // Ensures Vite uses the correct entry file
